12de63e9f2193f3092686937ea34d583395ee37c
M <38591075+M507@users.noreply.github.com>
2022-10-11 15:13:01 -0400

    Update and rename install.sh to Install.sh

---
+++ Install.sh
@@ -0,0 +1,10 @@
+echo "Create a cert" 
+openssl req -x509 -newkey rsa:4096 -nodes -out application/cert.pem -keyout application/key.pem -days 365 -subj "/C=US/ST=NY/L=NY/O=AAA Inc. /OU=IT Department/CN=AAA.com"
+echo "Install python3 dependencies" 
+sudo add-apt-repository ppa:deadsnakes/ppa -y
+sudo apt-get update -y
+# sudo apt-get install python3.7 -y
+# curl -O https://bootstrap.pypa.io/get-pip.py
+# sudo python3.7 get-pip.py
+apt install python3-pip -y
+pip3 install -r application/requirements.txt
--- install.sh
+++
@@ -1,8 +0,0 @@
-echo "Create a cert" 
-openssl req -x509 -newkey rsa:4096 -nodes -out application/cert.pem -keyout application/key.pem -days 365
-echo "Install python3 dependencies" 
-apt install python3-pip -y
-pip3 install -r application/requirements.txt
-# additions
-pip3 install boto3
-pip3 install awscli
